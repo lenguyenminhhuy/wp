daecf6270da924ef0c1ec271206fbfb540f1aa15
Le Mi <leemii1802@gmail.com>
2020-04-26 19:51:27 +0700

    3.1

--- a3/script.js
+++ a3/script.js
@@ -1,1 +1,26 @@
 /* Insert your javascript here */
+
+/* https://www.w3schools.com/jsref/prop_element_clientheight.asp */ // have a look at this site nha em
+
+let activeLink = document.querySelectorAll('.nav-item') // 
+let element = document.getElementsByTagName('nav') // get element which has tag name = nav, they are: about us, price, now showing
+let navbarHeight = element[0].clientHeight // height of navigation bar equal to the height of aboutus_clientheight 
+let aboutUs = document.getElementById('About-us') // assign variable aboutUs to get all the content in which the id = about-us
+let price = document.getElementById('Prices') // assign variable price to get all the content in which the id = Prices
+let nowshowing = document.getElementById('nowshowing') // assign variable nowshowing to get all the content in which the id = nowshowing
+let reachAboutUs = aboutUs.offsetTop // assign reachAboutUs: get the height to reach About us section: from the top of page to "About us" section
+let reachPrice = price.offsetTop // assign reachPrice: get the height to reach Price section: from the top of page to "Price" section
+let reachNowShowing = nowshowing.offsetTop // assign reachNowShowing: get the height to reach Now showing section: from the top of page to "now showing" section
+
+document.addEventListener('scroll', () => {
+    let scrollDistance = navbarHeight + window.scrollY // the real scroll long is equaled to the height of navigation bar + window scrolling 
+    if (reachAboutUs <= scrollDistance && scrollDistance < reachPrice) // if we scroll reach to the about us section and not have reached to price section yet
+        activeLink[0].classList.add('active') // the class of the div of this section will be added "active"
+    else activeLink[0].classList.remove('active') // otherwise, it will be disable
+    if (reachPrice <= scrollDistance && scrollDistance < reachNowShowing) // if we scroll reach to the price section and not have reached to nowshowing section yet
+        activeLink[1].classList.add('active') // the class of the div of this section will be added "active"
+    else activeLink[1].classList.remove('active') // otherwise, it will be disable
+    if (reachNowShowing <= scrollDistance) // if we scroll reach to the now showing section
+        activeLink[2].classList.add('active') // the class of the div of this section will be added "active"
+    else activeLink[2].classList.remove('active') // otherwise, it will be disable
+})
