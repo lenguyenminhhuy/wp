5f38f8e3c7c0bdc458a396d1f975c2cee3766ee0
Le Mi <leemii1802@gmail.com>
2020-05-28 22:16:35 +0700

    filter

---
+++ a5/cart.js
@@ -0,0 +1,52 @@
+$(document).ready(function (data) {
+    $('.cart_update').click(function () {
+        var action = "add";
+        var pid = $(this).attr("id");
+        var pname = $('#name' + pid).val();
+        var pbrand = $('#brand' + pid).val();
+        var pprice = $('#price' + pid).val();
+        var pquantity = $('#quantity' + pid).val();
+
+        if (pquantity > 0) {
+            $.ajax({
+                url: "action.php",
+                method: "POST",
+                dataType: "json",
+                data: {
+                    pid: pid,
+                    pname: pname,
+                    pbrand: pbrand,
+                    pprice: pprice,
+                    pquantity: pquantity,
+                    action: action
+                },
+                success: function (data) {
+                    $('#purchase_data').html(data.purchase_data);
+                    $('.badge').text(data.cart_item);
+                    alert("Product has been added into Cart. Click to Cart on the navbar to see your order.");
+                }
+            });
+        }
+        else {
+            alert("Please enter the number of quantity. (not zero) ")
+        }
+    });
+
+    $(document).on('keyup', '.quantity', function () {
+        var action = "quantity_change";
+        var pid = $(this).data("pid");
+        var quantity = $(this).val();
+
+        if (quantity != '') {
+            $.ajax({
+                url: "action.php",
+                method: "POST",
+                dataType: "json",
+                data: { pid: pid, quantity: quantity, action: action },
+                success: function (data) {
+                    $('#purchase_data').html(data.purchase_data);
+                }
+            });
+        }
+    });
+});  
--- a5/product.php
+++ a5/product.php
@@ -2,41 +2,24 @@
 <html lang="zxx">
 
 <head>
-	<title>The Plaza - eCommerce Template</title>
+	<title> Assignment 5 </title>
 	<meta charset="UTF-8">
 	<meta name="description" content="The Plaza eCommerce Template">
 	<meta name="keywords" content="plaza, eCommerce, creative, html">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- Favicon -->
 	<link href="img/favicon.ico" rel="shortcut icon" />
-
-	<!-- Google Fonts -->
-	<link href="https://fonts.googleapis.com/css?family=Raleway:400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-	<!-- Stylesheets -->
 	<link rel="stylesheet" href="css/bootstrap.min.css" />
 	<link rel="stylesheet" href="css/font-awesome.min.css" />
 	<link rel="stylesheet" href="css/owl.carousel.css" />
 	<link rel="stylesheet" href="style.css" />
 	<link rel="stylesheet" href="nav.css" />
-
 	<link rel="stylesheet" href="css/animate.css" />
-
-
-	<!--[if lt IE 9]>
-	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-
 </head>
 
 <body>
-	<!-- Page Preloder -->
 	<div id="preloder">
 		<div class="loader"></div>
 	</div>
-
-	<!-- Header section -->
 	<nav id="navigtion" class="sticky">
       <ul>
 	  <li><a class="nav-item nav-link" href="index.php">About Us</a></li>
@@ -45,10 +28,6 @@
         <li><a class="nav-item nav-link" href="contact.php"> Contact </a></li>
       </ul>
     </nav>
-	<!-- Header section end -->
-
-
-	<!-- Page Info -->
 	<div class="page-info-section page-info">
 		<div class="container">
 			<div class="site-breadcrumb">
@@ -60,10 +39,6 @@
 			<img src="img/page-info-art.png" alt="" class="page-info-art">
 		</div>
 	</div>
-	<!-- Page Info end -->
-
-
-	<!-- Page -->
 	<div class="page-area product-page spad">
 		<div class="container">
 			<div class="row">
@@ -71,7 +46,6 @@
 					<figure>
 						<img class="product-big-img" src="img/clothes/1.jpg" alt="">
 					</figure>
-				
 				</div>
 				<div class="col-lg-6">
 					<div class="product-content">
